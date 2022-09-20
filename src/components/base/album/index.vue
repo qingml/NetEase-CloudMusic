7cff889c0d26836934215971d99985aa71bf9ba2
paocaiyu96 <1477129230@qq.com>
2022-09-21 01:02:00 +0800

    fix(style): fix album background

--- src/components/base/album/index.vue
+++ src/components/base/album/index.vue
@@ -1,119 +1,120 @@
 <template>
   <ul class="album__block">
-    
-    <li class="album__block--item" 
-    v-for="(item,index) in data"
-    :key = "index"
-    @click="handleJumpDetail(item.id)"
+    <li
+      class="album__block--item"
+      v-for="(item, index) in data"
+      :key="index"
+      @click="handleJumpDetail(item.id)"
     >
       <div class="album__block--item-wrap">
-        <img :src="item.blurPicUrl" >
+        <img :src="item.blurPicUrl" />
         <div class="album__block--item-top">
           <i class="iconfont icon-bofang"></i>
           <span> {{ item.subType }}</span>
         </div>
         <div class="album__block--item-bottom">
           <span class="name ellipsis">
-            {{item.name}}
+            {{ item.name }}
           </span>
           <span class="artist-name">
-            {{item.artist.name}}
+            {{ item.artist.name }}
           </span>
           <span class="publish-time">
-            {{formatDate(item.publishTime)}}
+            {{ formatDate(item.publishTime) }}
           </span>
         </div>
       </div>
     </li>
-
   </ul>
 </template>
 
 <script setup>
- import { useRouter } from "vue-router";
+import { useRouter } from "vue-router";
 import { formatDate } from "@/utils/number";
 const router = useRouter();
 
 defineProps({
-  data:{
-    type:Array,
+  data: {
+    type: Array,
     default: () => [],
-  }
-})
+  },
+});
 
 const handleJumpDetail = (id) => {
- 
   router.push(`/album-detail/${id}`);
 };
 </script>
 
 <style lang="less">
 .album {
-  &__block{
+  &__block {
     height: 100%;
     width: 100%;
     display: flex;
     flex-wrap: wrap;
     justify-content: flex-start;
-    margin-left: 1%;
     margin-top: 20px;
     position: relative;
-    
 
-    &--item{
+    &--item {
       list-style-type: none;
-      padding: 0px  15px 90px 15px;
-      width: 16%;
+      padding: 0px 15px 90px 15px;
       cursor: pointer;
-      img{
+
+      img {
         width: 160px;
         height: 160px;
-        
       }
-      &-wrap{
-        z-index: 2;
-        
-      }
-      &::after{
-        top: 9px;
-        left: 32px;
-        position: absolute;
-        width: 150px;
-        height: 150px;
-        border-radius: 50%;
-        background-color: black;
-        content: "";
-        display: block;
-        &:hover{
-          left: 40px;
+
+      &-wrap {
+        z-index: 9;
+        height: 160px;
+
+        &::after {
+          z-index: -1;
+          top: 0px;
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          background-color: black;
+          content: "";
+          display: block;
+          left: 10px;
+          transition: left 0.5s;
         }
-        //left: 20px;
+
+        &:hover {
+          &::after {
+            left: 16px;
+          }
+        }
       }
-     &-bottom{
+
+      &-bottom {
         width: 100%;
-      
+
         display: flex;
         flex-direction: column;
-        justify-content: flex-start;  
+        justify-content: flex-start;
         position: absolute;
         bottom: -70px;
         //height: 40%;
 
-        >span{
+        > span {
           margin-top: 3px;
           font-size: 12px;
           color: #999;
-         
         }
 
-        .name{
+        .name {
           font-size: 14px;
-          color: #4A4A4A;
+          color: #4a4a4a;
           font-weight: bolder;
         }
-     }
+      }
 
-     &-top {
+      &-top {
         position: absolute;
         top: 5px;
         right: 14px;
@@ -126,13 +127,11 @@ const handleJumpDetail = (id) => {
         align-items: center;
         padding: 0 10px;
 
-        span{
+        span {
           font-weight: bolder;
         }
       }
+    }
   }
-  }
-
-  
 }
 </style>
